7f8b6fcc95a94d21593f31a02ac397a42d8c69b1
R. Mark Volkmann <m.volkmann@elsevier.com>
2011-11-09 16:03:21 -0600

    improved web UI

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # doctor
 
-Create documentation from a JavaScript AST.
+Creates documentation from a JavaScript AST.
 
 ## Installation
 
@@ -8,4 +8,20 @@ npm install doctor
 
 ## Usage
 
+To generate a report.json file in the output subdirectory:
+```
 doctor --debug myfile1.js myfile2.js
+```
+
+To serve dynamically generated documentation:
+```
+cp output/report.json view
+node lib/server.js
+open http://localhost:3000
+```
+
+To automatically generate doc.css when doc.scss is modified:
+```
+cd view
+sass --watch .:.
+```
--- view/js/doc.js
+++ view/js/doc.js
@@ -1,56 +1,79 @@
-$(function () {
-  function renderItems(report, itemKeys) {
-    $("#page").html('');
-    itemKeys.forEach(function (itemKey, i) {
+'use strict';
+/*global alert: false */
+
+var aa = {}; // APTO API namespace
+
+aa.loadReport = function () {
+  $.ajax({
+    dataType: 'json',
+    error: function (err) {
+      alert('problem loading report');
+      console.log(err);
+    },
+    success: aa.render,
+    url: 'report.json'
+  });
+};
+
+aa.render = function (report) {
+  // TODO: When does report sometimes have a property named "report"?
+  report = report.report ? report.report : report;
+
+  // Note that root is a "group".
+  var root = report.items.root;
+  aa.renderToc(report, root, $('#toc'));
+};
+
+aa.renderItems = function (report, itemKeys) {
+  var content = $('#content');
+  content.html('');
+
+  itemKeys.forEach(function (itemKey) {
+    var item = report.items[itemKey];
+
+    var div = $('<div id="' + itemKey + '" class="item">');
+    div.text(item.name);
+    content.append(div);
+
+    var type = item.type;
+    if (!item.api) {
+      type = 'private ' + type;
+    }
+    div = $('<div class="type">');
+    div.text(type);
+    content.append(div);
+  });
+};
+
+aa.renderToc = function (report, group, element) {
+  var ul = $('<ul>');
+  element.append(ul);
+
+  if (group.items) {
+    group.items.forEach(function (itemKey, i) {
       var item = report.items[itemKey];
-      console.log(itemKey);
-      $("#page").append('<h3 id="' + itemKey + '">' + item.name + '</h3>');
-      if (!item.api) {
-        $("#page").append('<span style="color:red">private</span>');
+      if (item.type !== 'function') {
+        var li = $('<li>');
+        ul.append(li);
+        //var a = $('<a href="javascript:void(0)">' + item.name + '</a>');
+        var a = $('<a href="#">' + item.name + '</a>');
+        li.append(a);
+
+        a.click(function () {
+          if (item.items) {
+            if (a.data('rendered')) {
+              a.nextAll().toggle();
+            } else {
+              aa.renderToc(report, item, li);
+              a.data('rendered', true);
+            }
+          }
+
+          aa.renderItems(report, item.items);
+        });
       }
     });
   }
+};
 
-  function renderToc(report, group, element) {
-    var ul = $("<ul>");
-    element.append(ul);
-    if (group.items) {
-      group.items.forEach(function (itemKey, i) {
-        var item = report.items[itemKey];
-        var li = $("<li>");
-        ul.append(li);
-        var a = $('<a href="JavaScript:void(0)">' + item.name + '</a>');
-        li.append(a);
-        a.click(function () {
-          if (item.items) {
-            if (!a.data('rendered')) {
-              renderToc(report, item, li);
-            }
-            a.data('rendered', true);
-            renderItems(report, item.items);
-          }
-        });
-      });
-    }
-  }
-
-  function render(report) {
-    report = report.report ? report.report : report;
-
-    var items = report.items;
-    var root = items.root;
-
-    renderToc(report, root, $("#toc"));
-  }
-
-  $.ajax({
-    dataType: 'json',
-    error: function (x) {
-      alert("problem loading report");
-    },
-    success: function (report) {
-      render(report);
-    },
-    url: "report.json"
-  });
-});
+$(aa.loadReport);
