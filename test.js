2c2bdae8fe0895dc78bd4cf6d700e645a5c9d6d7
Justin Deal <justin.deal@gmail.com>
2011-12-28 15:54:22 -0600

    added line & column numbers to ast

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+var t;
 /*
  * hello world
  */
--- test/fixture/define.ast.js
+++ test/fixture/define.ast.js
@@ -8,20 +8,32 @@ module.exports = {
         {
           "type": "name",
           "value": "foo",
+          "pos": 9,
+          "line": 1,
+          "column": 10,
           "comments": []
         },
         {
           "type": "parameters",
           "nodes": [],
+          "pos": 13,
+          "line": 1,
+          "column": 14,
           "comments": []
         },
         {
           "type": "nodes",
           "nodes": [],
+          "pos": 17,
+          "line": 2,
+          "column": 1,
           "comments": []
         }
       ],
-      "comments": []
+      "pos": 0,
+      "comments": [],
+      "line": 1,
+      "column": 1
     },
     {
       "type": "define-function",
@@ -29,6 +41,9 @@ module.exports = {
         {
           "type": "name",
           "value": "bar",
+          "pos": 29,
+          "line": 4,
+          "column": 10,
           "comments": []
         },
         {
@@ -37,14 +52,23 @@ module.exports = {
             {
               "type": "name",
               "value": "x",
+              "pos": 33,
+              "line": 4,
+              "column": 14,
               "comments": []
             },
             {
               "type": "name",
               "value": "y",
+              "pos": 36,
+              "line": 4,
+              "column": 17,
               "comments": []
             }
           ],
+          "pos": 33,
+          "line": 4,
+          "column": 14,
           "comments": []
         },
         {
@@ -59,29 +83,50 @@ module.exports = {
                     {
                       "type": "operator",
                       "value": "*",
+                      "pos": 52,
+                      "line": 5,
+                      "column": 12,
                       "comments": []
                     },
                     {
                       "type": "name",
                       "value": "x",
+                      "pos": 50,
+                      "line": 5,
+                      "column": 10,
                       "comments": []
                     },
                     {
                       "type": "name",
                       "value": "y",
+                      "pos": 54,
+                      "line": 5,
+                      "column": 14,
                       "comments": []
                     }
                   ],
+                  "pos": 50,
+                  "line": 5,
+                  "column": 10,
                   "comments": []
                 }
               ],
-              "comments": []
+              "pos": 43,
+              "comments": [],
+              "line": 5,
+              "column": 3
             }
           ],
+          "pos": 43,
+          "line": 5,
+          "column": 3,
           "comments": []
         }
       ],
-      "comments": []
+      "pos": 20,
+      "comments": [],
+      "line": 4,
+      "column": 1
     }
   ],
   "comments": []
